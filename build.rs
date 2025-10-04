5e54e498d51920b47ee21ccfcc20b8f88e6d3fb9
SakiiCode <13037299+SakiiCode@users.noreply.github.com>
2025-10-05 00:54:13 +0200

    check if env variables are set

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
 fn main() {
+    // these env variables must be set based on your installation
+    // put them into .cargo/config.toml
+    // https://github.com/SakiiCode/lv_bevy_ecs?tab=readme-ov-file#building-for-embedded
+    let _libclang_path = env!("LIBCLANG_PATH");
+    let _bindgen_extra_clang_args = env!("BINDGEN_EXTRA_CLANG_ARGS");
     embuild::espidf::sysenv::output();
 }

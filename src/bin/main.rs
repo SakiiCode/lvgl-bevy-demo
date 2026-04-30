f9ec19534b58bc7ac70d15bc916de6d60bad81b0
SakiiCode <13037299+SakiiCode@users.noreply.github.com>
2026-04-30 14:13:06 +0200

    add rgb led and defmt

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,18 +7,24 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
-use defmt::info;
+use defmt_serial as _;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+use esp_backtrace as _;
+use esp_hal::Blocking;
 use esp_hal::clock::CpuClock;
+use esp_hal::gpio::{Level, Output, OutputConfig};
 use esp_hal::timer::timg::TimerGroup;
-use {esp_backtrace as _, esp_println as _};
+use esp_hal::uart::{Config, Uart};
+use static_cell::StaticCell;
 
 extern crate alloc;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
+
+static SERIAL: StaticCell<Uart<'static, Blocking>> = StaticCell::new();
 
 #[allow(
     clippy::large_stack_frames,
@@ -31,24 +37,41 @@ async fn main(spawner: Spawner) -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+    let uart = Uart::new(peripherals.UART0, Config::default())
+        .unwrap()
+        .with_rx(peripherals.GPIO3)
+        .with_tx(peripherals.GPIO1);
+
+    let serial = SERIAL.init(uart);
+
+    serial.write(b"Hello world!\n").unwrap();
+
+    defmt_serial::defmt_serial(serial);
+    defmt::error!("Hello defmt!");
+
+    esp_alloc::heap_allocator!(size: 60 * 1024);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(timg0.timer0);
 
-    info!("Embassy initialized!");
+    defmt::info!("Embassy initialized!");
 
-    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
-    let (mut _wifi_controller, _interfaces) =
-        esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
-            .expect("Failed to initialize Wi-Fi controller");
+    // let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
+    // let (mut _wifi_controller, _interfaces) =
+    //     esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
+    //         .expect("Failed to initialize Wi-Fi controller");
+
+    let mut green_led = Output::new(peripherals.GPIO16, Level::High, OutputConfig::default());
 
     // TODO: Spawn some tasks
     let _ = spawner;
 
     loop {
-        info!("Hello world!");
+        defmt::info!("Hello world!");
         Timer::after(Duration::from_secs(1)).await;
+        green_led.set_low();
+        Timer::after(Duration::from_secs(1)).await;
+        green_led.set_high();
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
